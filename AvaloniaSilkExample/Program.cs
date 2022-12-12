bfd0e0385f8125d3ce5d0e3a9f9c597863535879
Nikita Tsukanov <keks9n@gmail.com>
2022-12-12 12:16:04 +0600

    Update avalonia

--- AvaloniaSilkExample/Program.cs
+++ AvaloniaSilkExample/Program.cs
@@ -18,6 +18,10 @@ namespace AvaloniaSilkExample
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+		.With(new Win32PlatformOptions()
+                {
+                    UseWgl = true,
+                })
                 .LogToTrace();
     }
 }
